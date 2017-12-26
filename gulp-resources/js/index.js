905721322756705407353b89dc730178b094fba0
Sander Van Hullebusch <sandervanhullebusch@gmail.com>
2017-12-26 18:57:01 +0100

    Small SEO fixes, font-sizes etc.

--- gulp-resources/js/index.js
+++ gulp-resources/js/index.js
@@ -25,13 +25,67 @@ $(document).ready(function(){
     $("#year").html((new Date).getFullYear());
     
     
-    // Owl carousel
-    //$('.carousel').slick({
-        // Hier komt de opmaak per viewport van de slick carousel
-    //});
-    
-    /*$('.project-images-carousel').slick({
-        
+    /*$('.carousel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    dots: false});/*,
+    responsive: [
+    {
+      breakpoint: 200,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplaySpeed: 1000,
+        infinite: true,
+        dots: false
+        }
+    },
+    {
+      breakpoint: 300,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        autoplaySpeed: 1000,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplaySpeed: 2000,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplaySpeed: 2000,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+      ]
     });*/
     
     $(".hamburger-menu").on('click', function(){
