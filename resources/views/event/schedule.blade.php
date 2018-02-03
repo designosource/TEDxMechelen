64a4127a071fa327b23fde16c3d39eb710cca3f2
Hanna Walgrave <Walgrave.hanna@hotmail.com>
2018-02-03 14:23:55 +0100

    schedule

--- resources/views/event/schedule.blade.php
+++ resources/views/event/schedule.blade.php
@@ -15,10 +15,10 @@
                     </div>
                     <div class="information">
                         <div class="type btn btn-regular all-caps">
-                            <p>Welcome Wim Soons</p>
+                            <p>Welcome</p>
                         </div>
                         <div class="description">
-                            <p>Introduction speech by Wim Soons , license holder of TEDxMechelen </p>
+                            <p>Introduction speech by Wim Soons , license holder of TEDxMechelen. </p>
                         </div>
                     </div>
                 </div>
@@ -32,9 +32,7 @@
                             <p>Bram Vanderborght</p>
                         </div>
                         <div class="description">
-                            <p>Bram Vanderborght is an absolute authority in the field of robotics. The top researcher has a master’s degree in mechanical engineering at the Vrije University of Brussels (VUB) with the highest distinction, and received his doctorate in applied sciences in 2007.Vanderborght conducted research at the Italian Institute of Technology in Genova, the Babes-Bolyai University Cluj-Napoca (Romania), and Japanese / French robo- tics laboratory in Tsukuba (Japan).
-                            Recently, his research on self-healing robots are still getting attention in the national media. His research interests include cognitive and physical human robot interaction, robot-assisted therapy, humanoids and rehabilitation robotics such as exoskeletons.
-                            </p>
+                            <p>Revolution in Robotics by Bram Vanderborght , professor robotics @ VUB.</p>
                         </div>
                     </div>
                 </div>
@@ -48,9 +46,8 @@
                             <p>Nico Huybrechts</p>
                         </div>
                         <div class="description">
-                            <p>Entrepreneurial professional with a background in Data Management, Analytics and Business Intelligence. Co-founder of dataSHIFT. Convinced that everything can be measured. People, performance, success. Historically, currently, predictively.
-                            His personal ambition is to make measuring motivational in order to lead to better business decisions and outcomes.</p>
-                        </div>
+                            <p>Talk DataSHIFT by Nico Huybrechts , founding Partner @ DataSHIFT.</p>
+                   </div>
                     </div>
                 </div>
 
@@ -60,11 +57,10 @@
                     </div>
                     <div class="information">
                         <div class="type btn btn-regular all-caps">
-                            <p>Lidewij Nuyten</p>
+                            <p>Lidewij Nuitten</p>
                         </div>
                         <div class="description">
-                            <p>Lidewij Nuitten is a television maker, who is the past couple of years made numerous amount of very intriguing series. Nuitten studied journalism at the Erasmushogeschool Brussel, and did an internship at Brussels Week. Then she went to work for the VRT, and she made the now famous reporter series ‘Iedereen Beroemd’. ‘Always straight’ - in which she walked in a straight line from Limburg until the coast - and ‘De Laatstejaars’.
-                            Her most recent work is ‘My Street’. Setup of this project was to interview as many of the people as possible in her very diverse street in Schaarbeek. Armed with 2 cameras she was able to capture the most fantastic stories.</p>
+                            <p>Talk revolution in reporting by Lidewij Nuitten , televisionmaker.</p>
                         </div>
                     </div>
                 </div>
@@ -78,8 +74,7 @@
                             <p>Geert Van Wonterghem</p>
                         </div>
                         <div class="description">
-                            <p>He has been many things in his life: a fanatic software develo- per, an innovation manager, a professor, ... But first & fore- most, he is and will always be an entrepreneur. He loves everything about innovation, has done a lot of innovating him- self and has spent a lot of time sparring with people on how to organize innovation within their company.
-                            He also loves change projects. To guide people in new directi- ons and see it happen.</p>
+                            <p>Talk revolutionary way to network via technology by Geert Van Wonterghem , founding partner @ MeetMatch.</p>
                         </div>
                     </div>
                 </div>
@@ -93,8 +88,8 @@
                             <p>Nilesh Mahdu</p>
                         </div>
                         <div class="description">
-                            <p>Nilesh Madhu is a researcher dedicated to speech technology and sound processing.
-                            He is an engineer from training and a professor at the University from Ghent.</p>
+                            <p>Talk revolution in sound processing by Nilesh Mahdu ,
+                                Professor @ University of Ghent.</p>
                         </div>
                     </div>
                 </div>
@@ -108,8 +103,8 @@
                             <p>Rik Vera</p>
                         </div>
                         <div class="description">
-                            <p>Having gathered years of C-level experience in sales and marketing related functions, he now inspires companies to develop customer-centric strategies fit for a connected world. Rik has given more than 350 keynote sessions around the world, in more than 15 countries, divided over 3 continents, to international pioneers like Assa Abloy, Cisco, Volvo, Swedbank, Telia, Baloise, Mastercard, Mundiphar- ma, Sogeti and Microsoft.
-                            He is a renowned thought-leader, worldwide keynote spea- ker, acclaimed author and trusted advisor on the topics of extreme customer-centricity, disruption, business model change, sales, marketing and making companies ready for the Day After Tomorrow.</p>
+                            <p>Talk managers the day after tomorrow by Rik vera ,
+                                CEO @ Nexxworks.</p>
                         </div>
                     </div>
                 </div>
